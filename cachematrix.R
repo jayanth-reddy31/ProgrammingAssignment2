a0ec9dda6696401ee41c9a9b56a1f6e3e5a5e361
Jayanth S <156926898+jayanth-reddy31@users.noreply.github.com>
2025-04-09 22:51:39 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,58 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions create a special matrix object that can cache its inverse.
+## This avoids repeated costly computations of the matrix inverse.
 
-## Write a short comment describing this function
-
+## makeCacheMatrix creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL  # initialize inverse as NULL
+  
+  # Set the matrix
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL  # reset inverse cache when matrix is changed
+  }
+  
+  # Get the matrix
+  get <- function() x
+  
+  # Set the inverse
+  setInverse <- function(inverse) inv <<- inverse
+  
+  # Get the inverse
+  getInverse <- function() inv
+  
+  # Return a list of all the above functions
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve computes the inverse of the special "matrix" returned by makeCacheMatrix.
+## If the inverse has already been calculated and cached, it retrieves it from the cache.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  
+  # If inverse already exists in cache, return it
+  if (!is.null(inv)) {
+    message("getting cached inverse")
+    return(inv)
+  }
+  
+  # If inverse not cached, compute it
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setInverse(inv)  # Cache the inverse
+  inv
 }
+# Create a matrix
+mat <- matrix(c(2, 1, 1, 2), nrow = 2, ncol = 2)
+
+# Create the special matrix object
+cachedMat <- makeCacheMatrix(mat)
+
+# Compute and cache the inverse
+inv1 <- cacheSolve(cachedMat)
+
+# Retrieve the cached inverse (should show the message)
+inv2 <- cacheSolve(cachedMat)
+
